b9040a0c67f543b904ba76c6678ea61edf2de51e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 09:43:07 +0900

    using router for NavBar.vue

--- components/layouts/NavBar.vue
+++ components/layouts/NavBar.vue
@@ -5,7 +5,7 @@
         <b-nav-item href="#">Ruby Online</b-nav-item>
 
         <b-nav-item-dropdown text="Menu" right>
-          <b-dropdown-item href="#">About</b-dropdown-item>
+          <b-dropdown-item to="/about">About</b-dropdown-item>
           <b-dropdown-item href="#">Contact</b-dropdown-item>
           <b-dropdown-item href="#">Code</b-dropdown-item>
         </b-nav-item-dropdown>
