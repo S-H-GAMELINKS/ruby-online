7f23679b29943f6d939d5d49384b6e31c3def527
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 11:47:24 +0900

    run ruby code

--- pages/code.vue
+++ pages/code.vue
@@ -1,68 +1,58 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ruby-online
-      </h1>
-      <h2 class="subtitle">
-        Run your Ruby code in PWA
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+    <div class="container">
+        <editor v-model="code" @init="editorInit" lang="ruby" theme="chrome" width="500" height="100"></editor>
+        <button v-on:click="runCode">run</button>
+
+        <p>Status : {{ this.status.status }}</p>
+        <p v-if='this.status.compiler_error != ""'>Error : {{ this.status.compiler_error }}</p>
+        <p v-if='this.status.compiler_message != ""'>Compiler Messgae : {{ this.status.compiler_message }}</p>
+        <p> Program Message : </p>
+        <p v-if='this.status.program_message != ""' v-html="this.status.program_message"></p>
+        <p v-if='this.status.program_output != ""'> Program OutPut : {{ this.status.program_output }}</p>
     </div>
-  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    Logo
-  }
+    data: function() {
+        return {
+            status: {
+                compiler_error: "",
+                compiler_message: "",
+                program_message: "",
+                program_output: "",
+                status: 0
+            },
+            code: ""
+        }
+    },
+    components: {
+        editor: require('vue2-ace-editor')
+    },
+    methods: {
+        editorInit: function() {
+            require('brace/ext/language_tools')
+            require('brace/mode/html')                
+            require('brace/mode/ruby')
+            require('brace/mode/less')
+            require('brace/theme/chrome')
+            require('brace/snippets/ruby')
+        },
+        runCode: function() {
+            axios.defaults.headers['Content-type'] = 'application/json'
+
+            axios.post("https://wandbox.org/api/compile.json", { "code":this.code,"options": "", "compiler": "ruby-2.5.1", "compiler-option-raw": "" }).then((response) => {
+                console.log(response)
+                this.status.compiler_error = response.data.compiler_message
+                this.status.compiler_message = response.data.compiler_message
+                this.status.program_message = response.data.program_message.replace(/\n/g, "<br>")
+                this.status.status = response.data.status 
+            }, (error) => {
+                console.log(error);
+            })
+        }
+    }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
