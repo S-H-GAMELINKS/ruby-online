5f12de1c0b650fa11d79a146c90737ef1c2a2d72
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 09:44:26 +0900

    add router code for NavBar.vue

--- components/layouts/NavBar.vue
+++ components/layouts/NavBar.vue
@@ -7,7 +7,7 @@
         <b-nav-item-dropdown text="Menu" right>
           <b-dropdown-item to="/about">About</b-dropdown-item>
           <b-dropdown-item to="/contact">Contact</b-dropdown-item>
-          <b-dropdown-item href="#">Code</b-dropdown-item>
+          <b-dropdown-item to="/code">Code</b-dropdown-item>
         </b-nav-item-dropdown>
 
       </b-navbar-nav>
