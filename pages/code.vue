050fa3a4dd3ffcdf044eac422a00eaa213dfb019
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 12:12:17 +0900

    fix output

--- pages/code.vue
+++ pages/code.vue
@@ -1,14 +1,10 @@
 <template>
     <div class="container">
-        <editor v-model="code" @init="editorInit" lang="ruby" theme="chrome" width="500" height="100"></editor>
+        <editor v-model="code" @init="editorInit" lang="ruby" theme="twilight" width="500" height="100"></editor>
         <button v-on:click="runCode">run</button>
 
-        <p>Status : {{ this.status.status }}</p>
-        <p v-if='this.status.compiler_error != ""'>Error : {{ this.status.compiler_error }}</p>
-        <p v-if='this.status.compiler_message != ""'>Compiler Messgae : {{ this.status.compiler_message }}</p>
-        <p> Program Message : </p>
-        <p v-if='this.status.program_message != ""' v-html="this.status.program_message"></p>
-        <p v-if='this.status.program_output != ""'> Program OutPut : {{ this.status.program_output }}</p>
+        <p> Output : </p>
+        <div class="gray" v-if='this.status.program_message != ""' v-html="this.status.program_message"></div>
     </div>
 </template>
 
@@ -19,11 +15,7 @@ export default {
     data: function() {
         return {
             status: {
-                compiler_error: "",
-                compiler_message: "",
                 program_message: "",
-                program_output: "",
-                status: 0
             },
             code: ""
         }
@@ -37,18 +29,14 @@ export default {
             require('brace/mode/html')                
             require('brace/mode/ruby')
             require('brace/mode/less')
-            require('brace/theme/chrome')
+            require('brace/theme/twilight')
             require('brace/snippets/ruby')
         },
         runCode: function() {
             axios.defaults.headers['Content-type'] = 'application/json'
 
             axios.post("https://wandbox.org/api/compile.json", { "code":this.code,"options": "", "compiler": "ruby-2.5.1", "compiler-option-raw": "" }).then((response) => {
-                console.log(response)
-                this.status.compiler_error = response.data.compiler_message
-                this.status.compiler_message = response.data.compiler_message
                 this.status.program_message = response.data.program_message.replace(/\n/g, "<br>")
-                this.status.status = response.data.status 
             }, (error) => {
                 console.log(error);
             })
@@ -56,3 +44,12 @@ export default {
     }
 }
 </script>
+
+<style>
+.gray {
+    background-color: dimgray;
+    color: azure;
+    padding: 10px;
+}
+</style>
+>
