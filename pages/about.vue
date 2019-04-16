bc1d84c96f9705c3d8962c89bde45afeaf629e07
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 10:33:13 +0900

    fix about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,17 +1,22 @@
 <template>
-<div class="container">
-  <b-card>
-    <b-media>
+    <div>
+        <b-jumbotron>
+            <b-card>
+                <b-media>
+                    <img slot="aside" src="~/assets/ruby.png" width="64" height="64" alt="placeholder">
 
-      <h5 class="mt-0">Ruby Online</h5>
-      <p>
-          <b-link to="https://wandbox.org/">Wandbox</b-link>のAPIを使用してRubyのコードを実行できるWebサービスです！
-      </p>
-      <p>
-        PWAとしてインストールすることも可能です！
-      </p>
-
-    </b-media>
-  </b-card>
-</div>
+                    <h5 class="mt-0">Ruby Online</h5>
+                    <p>
+                        <b-link href="https://wandbox.org/">WandBox</b-link>のAPIを使用し、Rubyのコードを実行できるWebサービスです
+                    </p>
+                    <p>
+                        またPWAアプリとしてスマートフォンへインストールすることも可能です。
+                    </p>
+                    <p>
+                        Androidの場合は、Chromeで開き、「ホーム画面へ追加」、iOSの場合はSafariで開き、「ホーム画面へ追加」でインストールできます
+                    </p>
+                </b-media>
+            </b-card>
+        </b-jumbotron>
+    </div>
 </template>
