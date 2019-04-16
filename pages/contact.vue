7000fb0d3e369a2e632f2c4998545c9dd80cbf48
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 10:50:36 +0900

    fix contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,68 +1,26 @@
 <template>
-  <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        ruby-online
-      </h1>
-      <h2 class="subtitle">
-        Run your Ruby code in PWA
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+        <b-jumbotron>
+            <b-card>
+                <b-media>
+                    <h5 class="mt-0">Developer</h5>
+
+                    <b-media>
+                        <img slot="aside" src="~/assets/icon.png" width="64" height="64" alt="placeholder" />
+
+                        <h5 class="mt-0">S.H.</h5>
+                        <p class="mb-0">
+                            Skills : C/C++/Ruby/Rails/Vue.js/Stimulus/Rust
+                        </p>
+                        <p class="mb-0">
+                            Mail : gamelinks007@gmail.com
+                        </p>
+                        <p class="mb-0">
+                            GitHub : <b-link href="https://github.com/S-H-GAMELINKS">S-H-GAMELINKS</b-link>
+                        </p>
+                    </b-media>
+                </b-media>
+            </b-card>
+        </b-jumbotron>
     </div>
-  </section>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
