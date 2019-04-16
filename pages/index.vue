9767804d1088061035a0b61a020a0cfbc7ed0789
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 09:51:17 +0900

    using b-link

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <b-jumbotron header="Ruby Online" lead="Rubyのコードをスマホから試せるWebサービスです">
-        <p>サービスの詳細にかんしては<router-link to="/about">こちら</router-link></p>
+        <p>サービスの詳細にかんしては<b-link to="/about">こちら</b-link></p>
         <b-button variant="primary" to="/code">実際にコードを書いてみる</b-button>
       </b-jumbotron>
     </div>
