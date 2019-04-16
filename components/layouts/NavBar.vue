c2871ebc8d00ce5b6b581cdbfe4b249dcadb44f3
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-16 09:44:41 +0900

    add link to root path

--- components/layouts/NavBar.vue
+++ components/layouts/NavBar.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar type="dark" variant="dark">
       <b-navbar-nav>
-        <b-nav-item href="#">Ruby Online</b-nav-item>
+        <b-nav-item to="/">Ruby Online</b-nav-item>
 
         <b-nav-item-dropdown text="Menu" right>
           <b-dropdown-item to="/about">About</b-dropdown-item>
